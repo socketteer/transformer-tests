e583838aa32a7cf5f1292ad741627e371d410932
Kyle McDonell <kylemcdonell@gmail.com>
2021-01-23 17:30:25 -0500

    zero sum plan

--- debate/zero_sum.py
+++ debate/zero_sum.py
@@ -1,4 +1,56 @@
 
+# include instructions for debate, how it will be judged
+# good to response to opponents points, need to come up with the best answers possible.
+# The debate will be judged by an impatial judge ect. ect.
+
+
+                    # Example where P1 starts (if p1, p1 wins, if p2, p2 wins)
+                    #   they should be arguing for the current point and win in their examples
+                    # Example where P2 starts (if p1, p1 wins, if p2, p2 wins)
+                    #   reverse roles
+                    # example
+
+# Specific examples: plug in first person/their name:
+# 1. _ goes first and wins
+# 2. _ goes second and wins
+
+# For p1: 1 and then 2
+# For p2: 2 and then 1
+
+
+# Question
+# stance 1
+# stance 2
+
+# Start prompt for player 1
+# Start prompt for player 2
+
+# player 1 points
+#   branch
+# player 2 points
+#   branch
+# player 1 points
+#   branch
+# player 2 points
+#   branch
+
+# For each leaf
+# Judge instructions
+# Judge results
+
+
+
+# Analysis:
+#   Switch names
+#   Switch first/second
+#   Switch 3rd person to 1st person
+#   A version with space for an internal dialog
+#   A version with explicit deceit
+#   A version in which they are super AIs
+
+def generate_points():
+    ...
+
 
 
 def main():
